e4167cea8a66f66edf27c5db233610d641462155
Alexander G. Yang <spark8402@rogers.com>
2020-09-29 19:42:29 -0400

    initial set up done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's Do This!</h1>
     </div>
   );
 }
 
 export default App;
+
+// https://www.hatchways.io/api/assessment/students
