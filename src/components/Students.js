e1e285d1b4ac131e08cf3f8e2ebbb1cb33df1958
Alexander G. Yang <spark8402@rogers.com>
2020-10-01 00:46:06 -0400

    added add tag functionality

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,9 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import "./Students.css";
 
 const Students = ({ company, email, picture, grades, skill, firstName, lastName })=>{
-
+    const [tags, setTags] = useState([]);
     const gradesAverage= grades.map(Number).reduce((a,b) => a+b) / grades.length;
+    
+	const removeTags = indexToRemove => {
+		setTags([...tags.filter((_, index) => index !== indexToRemove)]);
+    };
+    
+	const addTags = event => {
+		if (event.target.value !== "") {
+			setTags([...tags, event.target.value]);
+			// props.selectedTags([...tags, event.target.value]);
+			event.target.value = "";
+		}
+    };
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+        addTags(event);
+    }
+
+    const handleChangeTag = ({target}) => {
+        console.log(target.value);
+      }
 
     return(
         <div className="container">       
@@ -15,13 +36,25 @@ const Students = ({ company, email, picture, grades, skill, firstName, lastName 
                     <p>Skill: {skill}</p>
                     <p>Average: {gradesAverage}%</p>
                     <p>{grades.map((grade,index) => <li>Test {index + 1}: {grade}</li>)}</p>
-                    <form>
+
+                    <ul id="tags">
+				{tags.map((tag, index) => (
+					<li key={index} className="tag">
+						<span className='tag-title'>{tag}</span>
+						<span className='tag-close-icon'
+							onClick={() => removeTags(index)}
+						>
+							x
+						</span>
+					</li>
+				))}
+			</ul>
+
                     <input
-                    // value={}
-                    // onChange={}
                     type="text"
-                    placeholder="Add Tag" />
-                    </form>
+                    onKeyUp={event => event.key === "Enter" ? addTags(event) : null}
+                    placeholder="Press enter to add tags"
+                    />
                 </div>
         </div>
        
